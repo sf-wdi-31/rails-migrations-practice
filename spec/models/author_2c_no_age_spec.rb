1e64b5496f2bb296cfe5ee7588bf8d51b29143d1
tgaff <tgaff@alumni.nd.edu>
2016-10-02 13:50:17 -0700

    2c: spec added

---
+++ spec/models/author_2c_no_age_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  describe 'pre-existing columns' do
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:email).of_type(:string) }
+    it { should have_db_column(:bio).of_type(:text) }
+  end
+
+  describe 'removed columns' do
+    it { should_not have_db_column(:address) }
+
+    it { should_not have_db_column(:age) }
+  end
+end
