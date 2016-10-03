8aeb12b4fa4c867d88d5156e72165c9f14602481
tgaff <tgaff@alumni.nd.edu>
2016-10-02 23:10:12 -0700

    Ex4 - admin and name spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,7 @@ RSpec.describe User, type: :model do
   it { should have_db_column(:name).of_type(:string) }
   it { should have_db_column(:phone).of_type(:integer) }
   it { should have_db_column(:address).of_type(:string) }
+
+  it { should have_db_column(:name).with_options(null: false) }
+  it { should have_db_column(:admin).of_type(:boolean).with_options(default: false) }
 end
