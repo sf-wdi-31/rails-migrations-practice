bf223debab228e3bfdc1c95c23c2cffb7f07f0bc
tgaff <tgaff@alumni.nd.edu>
2016-10-02 22:40:57 -0700

    Ex3 - added tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_db_column(:name).of_type(:string) }
-  it { should have_db_column(:phone).of_type(:integer) }
-  it { should have_db_column(:address).of_type(:string) }
+  describe "unchanged columns" do # note: this is an unusual use of describe, we're doing it here to help students out.
+    it { should have_db_column(:name).of_type(:string) }
+  end
+
+  describe "altered and new columns" do
+    it { should have_db_column(:phone).of_type(:string) }
+    it { should have_db_column(:street_address).of_type(:string) }
+    it { should have_db_column(:street_address2).of_type(:string) }
+    it { should have_db_column(:state).of_type(:string) }
+    it { should have_db_column(:country).of_type(:string) }
+    it { should have_db_column(:zip).of_type(:integer) }
+  end
+
+
+  # this is also an unsual spec - for this exercise all changes should be done in one migration
+  it "only has one additional migration file added" do
+    migration_files = Dir.glob("db/migrate/*.rb") # an array of all the files in db/migrate
+    expect(migration_files.length).to eq 3
+  end
 end
